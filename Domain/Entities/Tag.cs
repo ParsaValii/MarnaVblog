de9dde428a3a1d17c84f25cbb0ae4cad1098dfaf
ParsaValii <parsa.valizadee@gmail.com>
2024-02-16 19:54:11 +0330

    TagAdded

---
+++ Domain/Entities/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public ICollection<BlogPost> BlogPosts { get; set; }
+    }
+}
