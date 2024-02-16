12aa93fa648a8666cb2b86bfc696bd1f5bfa3f12
ParsaValii <parsa.valizadee@gmail.com>
2024-02-16 19:54:22 +0330

    ContextAdded

---
+++ Domain/MarnaDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class MarnaDbContext : DbContext
+    {
+        public MarnaDbContext() { }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+            .UseSqlServer(
+                "Server=localhost;Database=VblogMarnaDb;Integrated Security=True;TrustServerCertificate=True;"
+            );
+        }
+        public MarnaDbContext(DbContextOptions<MarnaDbContext> options) : base(options) { }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
---
+++ Domain/MarnaDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class MarnaDbContextFactory : IDesignTimeDbContextFactory<MarnaDbContext>
+    {
+        public MarnaDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<MarnaDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost;Database=Parsadb;Integrated Security=True;TrustServerCertificate=True;");
+
+            return new MarnaDbContext(optionsBuilder.Options);
+        }
+    }
+}
