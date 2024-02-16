74ce54a90ccf053fb10e7c498e28abd878cc5ee5
ParsaValii <parsa.valizadee@gmail.com>
2024-02-16 19:54:04 +0330

    BlogPostAdded

---
+++ Domain/Entities/BlogPost.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        public string Heading { get; set; }
+
+        public string PageTitle { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortDescrption { get; set; }
+
+        public string FeatureImageUrl { get; set; }
+
+        public string UrlHandle { get; set; }
+
+        public DateTime PublisheDate { get; set; }
+
+        public string Author { get; set; }
+
+        public bool Visible { get; set; }
+
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
