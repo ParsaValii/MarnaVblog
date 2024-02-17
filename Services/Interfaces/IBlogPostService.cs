a87f9384deb8772670c8b969257db05988f35380
ParsaValii <parsa.valizadee@gmail.com>
2024-02-17 15:59:17 +0330

    IBlogService&BlogServiceAdded

---
+++ Services/Interfaces/IBlogPostService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Interfaces
+{
+    public interface IBlogPostService : IDisposable
+    {
+        Task<IEnumerable<BlogPost>> GetAllBlogPostsAsync();
+
+        Task<BlogPost> GetBlogPostAsync(Guid id);
+
+        Task<BlogPost> InsertBlogPostAsync(BlogPost BlogPost);
+
+        BlogPost? UpdateBlogPostAsynce(BlogPost BlogPost);
+
+        Task<BlogPost?> DeleteBlogPostAsynce(Guid id);
+        BlogPost? DeleteBlogPostAsynce(BlogPost BlogPost);
+        Task SaveAsync();
+    }
+}
---
+++ Services/Services/BlogPostService.cs
@@ -0,0 +1,68 @@
+﻿using Domain.Entities;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services
+{
+    public class BlogPostService : IBlogPostService
+    {
+        private readonly MarnaDbContext _context;
+
+        public BlogPostService(MarnaDbContext marnaDbContext)
+        {
+            _context = marnaDbContext;
+        }
+
+        public async Task<IEnumerable<BlogPost>> GetAllBlogPostsAsync()
+        {
+            return await _context.BlogPosts.ToListAsync();
+        }
+
+        public async Task<BlogPost> GetBlogPostAsync(Guid id)
+        {
+            var BlogPost = await _context.BlogPosts.FindAsync(id);
+            return BlogPost;
+        }
+
+        public async Task<BlogPost> InsertBlogPostAsync(BlogPost BlogPost)
+        {
+            await _context.BlogPosts.AddAsync(BlogPost);
+            return BlogPost;
+        }
+
+        public BlogPost? UpdateBlogPostAsynce(BlogPost BlogPost)
+        {
+            _context.Entry(BlogPost).State = EntityState.Modified;
+            return BlogPost;
+        }
+
+        public async Task<BlogPost?> DeleteBlogPostAsynce(Guid id)
+        {
+            var BlogPost = await GetBlogPostAsync(id);
+            DeleteBlogPostAsynce(BlogPost);
+            return BlogPost;
+        }
+
+        public BlogPost? DeleteBlogPostAsynce(BlogPost BlogPost)
+        {
+            _context.Entry(BlogPost).State = EntityState.Deleted;
+            return BlogPost;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
